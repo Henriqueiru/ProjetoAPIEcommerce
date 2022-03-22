41d67c604329f7e6fae2784c47ff066f8cd6ab59
Carlos Encine <contato@carlosencine.com>
2022-03-21 23:49:20 -0300

    Update ProdutoCreateCommand.cs

--- Domain/Command/ProdutoCreateCommand.cs
+++ Domain/Command/ProdutoCreateCommand.cs
@@ -1,7 +1,11 @@
 using System;
 using MediatR;
+// aqui deixa uma linha em branco pra separar os usings do resto
 namespace ProjetoAPIEcommerce.Domain.Command
 {
+  // Nome da classe deve ser em inglês
+  // A pasta command não deve ficar dentro de Domain ela tem que icar em Application
+  // pra simpliicar renomeia para Product, retira a implementação da inteface e deixa essa classe ora da pasta command e dentro da classe Domain 
   public class ProdutoCreateCommand : IRequest<string>
   {
     public string Name { get; private set; }
