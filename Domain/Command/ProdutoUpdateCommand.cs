44ee3390bebd904d0622820b2bb480925bdf585b
Carlos Encine <contato@carlosencine.com>
2022-03-21 23:53:33 -0300

    Update ProdutoUpdateCommand.cs

--- Domain/Command/ProdutoUpdateCommand.cs
+++ Domain/Command/ProdutoUpdateCommand.cs
@@ -2,6 +2,7 @@
 using MediatR;
 namespace ProjetoAPIEcommerce.Domain.Command
 {
+  // Essa classe é desnecessária, uma evz que tu ja tem a entidade Product que tu pode reaproveitar
   public class ProdutoUpdateCommand : IRequest<string>
   {
     public int Id { get; private set; }
