2e12b08b2b4d2ba7217c216bebefbd3a1e18b032
Carlos Encine <contato@carlosencine.com>
2022-03-22 08:11:19 -0300

    Update ProdutosController.cs

--- Controllers/ProdutosController.cs
+++ Controllers/ProdutosController.cs
@@ -9,22 +9,27 @@ namespace ProjetoAPIEcommerce.Controllers
 {
   [Route("api/[controller]")]
   [ApiController]
+  // Nome da classe deve estar em inglês
   public class ProdutosController : ControllerBase
   {
     private readonly IMediator _mediator;
+    // não precisa injetar o repository aqui, uma vez que isso vai ser feito no respectivo command e usa prop ao inves dessa declaração de variavel tipada
     private readonly IRepository<Produto> _repository;
 
     public ProdutosController(IMediator mediator, IRepository<Produto> repository)
     {
+      
       this._mediator = mediator;
       this._repository = repository;
     }
 
     [HttpGet]
+    // usar nomes mais declarativos para metodos
     public async Task<IActionResult> Get()
     {
       return Ok(await _repository.GetAll());
     }
+    // seguindo o comentario anterior evitar a repetição de nomes de metodos, pois isso gera conflito
     [HttpGet("{id}")]
     public async Task<IActionResult> Get(int id)
     {
